21e6879d53a572c910effb5fccf158fc275d1b59
inkcherry <344119449@qq.com>
2017-11-21 20:50:19 +0800

    add  deque learn

--- main.cpp
+++ main.cpp
@@ -3,22 +3,28 @@
 #include "IK_Algorithm.h"
 #include "IK_Vector.h"
 #include "IK_List.h"
-
+#include "IK_Deque.h"
 int main()
 {
 	IK_STL::vector<int>test_vec;
 	test_vec.push_back(3);
 
-	IK_STL::list<int>test_list;
-	int arr[5] = { 3,2,434,534,1 };
-	for (int i = 0; i < 5; i++)
-		test_list.push_back(arr[i]);
-	test_list.sort();
-	IK_STL::LI_ITER::listIterator<int> ite = test_list.begin();
-	for (; ite != test_list.end(); ++ite)
-	{
-		std::cout << *ite<<" ";
-	}
+	//IK_STL::list<int>test_list;
+	//int arr[5] = { 3,2,434,534,1 };
+	//for (int i = 0; i < 5; i++)
+	//	test_list.push_back(arr[i]);
+	//test_list.sort();
+	//IK_STL::LI_ITER::listIterator<int> ite = test_list.begin();
+	//for (; ite != test_list.end(); ++ite)
+	//{
+	//	std::cout << *ite<<" ";
+	//}
+	IK_STL::deque<int>test_deque;
+	
+
+
+
+
 
 	std::cin.get();
 	return 0;
